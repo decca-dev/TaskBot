e9f75e84ad4a858eb086fc91cfdd408cd3a037aa
decca_dev <medcherbani@gmail.com>
2021-03-17 17:13:36 +0000

    init

---
+++ src/Commands/Bot/ping.ts
@@ -0,0 +1,14 @@
+import { Command } from '../../Interfaces';
+import { MessageEmbed } from 'discord.js';
+
+export const command: Command = {
+    name: "ping",
+    description: "Displays the api's latency and the bot's response time",
+    aliases: ['pong'],
+    usage: "ping",
+    category: "Bot",
+    devOnly: false,
+    run: async (client, message, args) => {
+        message.channel.send(`API Ping: ${client.ws.ping} ms`)
+    }
+}
---
+++ src/Events/ready.ts
@@ -0,0 +1,9 @@
+import { Event } from "../Interfaces";
+
+export const event: Event = {
+    name: "ready",
+    run: (client) => {
+        console.log(`${client.user.username} is online`);
+        client.user.setActivity('with Typescript!');
+    }
+}
